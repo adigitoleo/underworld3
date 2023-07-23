c5571cf3ee0e1aa20f19b7e8b1dc7c0ba0fa3996
maxzwilliams <59757812+maxzwilliams@users.noreply.github.com>
2023-07-24 00:20:14 +1000

    update

--- Jupyterbook/Notebooks/benchmarking/case2/runC.py
+++ Jupyterbook/Notebooks/benchmarking/case2/runC.py
@@ -47,8 +47,8 @@ tempMax   = 1.
 viscosity = 1
 
 tol = 1e-5
-res = 12                        ### x and y res of box
-nsteps = 10                 ### maximum number of time steps to run the first model 
+res = 96                        ### x and y res of box
+nsteps = 50                 ### maximum number of time steps to run the first model 
 epsilon_lr = 1e-3              ### criteria for early stopping; relative change of the Vrms in between iterations  
 
 
